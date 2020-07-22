7b296adf33c43bfa6b9c5410f30102dac0c6e55b
Shivek-cmd <59970948+Shivek-cmd@users.noreply.github.com>
2020-07-22 16:45:07 +0530

    Create files.cpp

---
+++ files.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class File{
+public:
+    string file_name;
+    double file_size;
+
+    File(string name, double size){
+        file_name=name;
+        file_size=size;
+    }
+    
+    File(){
+        file_name="";
+        file_size=0;
+    }
+};
