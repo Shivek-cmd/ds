c9fba36184a8210dcd9ccace28dc90c4e3995165
Shivek-cmd <59970948+Shivek-cmd@users.noreply.github.com>
2020-07-22 16:47:00 +0530

    Create linux.cpp

---
+++ linux.cpp
@@ -0,0 +1,35 @@
+#include "btree.hpp"
+
+//for any OS we can add , delete ,search files
+class linux{
+public:
+    BTree linux_btree;
+    linux(){
+        //every new OS will have it's btree object
+        linux_btree = BTree(3);
+    }
+    //operations on the file
+    void add_file(string, double);
+    BTreeNode * search_file(string , double);
+    void show_files();
+    void  remove(File );
+};
+
+
+void linux :: add_file(string name, double size){
+    File temp=File(name , size);
+    linux_btree.insert(temp);
+}
+
+BTreeNode* linux::search_file(string name, double size){
+    File temp = File(name, size);
+    return os_btree.search(temp);
+}
+
+void linux::show_files(){
+    os_btree.traverse();
+}
+
+void linux::remove(File file){
+    linux_btree.remove(file);
+}
